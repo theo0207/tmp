f82bd1572467487ec4d2ba2ff683f9a3f73bc083
theo0207 <theo@42maru.com>
2021-12-07 13:12:26 +0900

    :feat: Add:get search template

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Optional
 from fastapi import FastAPI, Depends
 
 from utils.auth import verify_token
+from core.es_client import es
 
 
 app = FastAPI()
@@ -23,12 +24,22 @@ async def shutdown():
     close_elasticsearch()
 
 
-@app.get("/search", dependencies=[Depends(verify_token)])
+@app.post("/search", dependencies=[Depends(verify_token)])
 def search():
-    pass
+    body = {
+        "params": {
+            "condition": False,
+            "query_string": "hello world",
+            "from": 20,
+            "size": 10
+        }
+    }
+    template = es.client.render_search_template(id="my-search-template", body=body)
+    query = template['template_output']['query']
+    es.client.search(index="some_index", query=query)
 
 
-@app.get("/export", dependencies=[Depends(verify_token)])
+@app.post("/export", dependencies=[Depends(verify_token)])
 def export():
     pass
 
--- utils/auth.py
+++ utils/auth.py
@@ -3,6 +3,7 @@ from fastapi import Header, HTTPException
 auth = True
 
 def verify_token(authorization: str = Header(None)):
+    print(authorization)
     if not authorization:
         raise HTTPException(status_code=401, detail="Token does not exist")
     if authorization != "something":
@@ -14,5 +15,5 @@ def decrypt():
 def check_identification_num():
     pass
 
-def check_session_time():
+def check_expired_time():
     pass
